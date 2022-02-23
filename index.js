82b182c52e59f6d8abde51349e68e2e2a2a1b468
jake langhel <jacobtlanghel@gmail.com>
2022-02-22 21:13:50 -0500

    add media queries

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 const navToggle = document.querySelector(".nav-toggle");
-const navBtns = document.querySelectorAll(".nav-btn");
+const dropDownBtns = document.querySelectorAll(".drop-down-btn");
 
 const openNav = () => {
     document.body.classList.toggle("nav-open");
 };
 
-const toggleDropDown = (e) => {
+const toggleArrow = (e) => {
+    const results = [];
     let target = e.target;
+    let arrow = null;
+    let innerTxt = null;
     if (target.classList.contains("arrow-img")) {
-        target = target.parentNode;
+        arrow = target;
+        target = arrow.parentNode;
+        innerTxt = target.innerText.toLowerCase();
+    } else {
+        innerTxt = target.innerText.toLowerCase();
+        const className = `${innerTxt}-arrow`;
+        arrow = document.querySelector(`.${className}`);
     }
-    const innerTxt = target.innerText.toLowerCase();
+    arrow.classList.toggle("toggle-arrow");
+    results.push(target);
+    results.push(innerTxt);
+    return results;
+};
+
+const toggleDropDown = (e) => {
+    const [target, innerTxt] = toggleArrow(e);
     const dropDowns = document.querySelectorAll(".container-drop-down");
     dropDowns.forEach((element) => {
         if (element.id === innerTxt) {
             element.classList.toggle("open-drop-down");
-            console.log(element);
         }
     });
 };
 
 navToggle.addEventListener("click", openNav);
-navBtns.forEach((btn) => {
+dropDownBtns.forEach((btn) => {
     btn.addEventListener("click", toggleDropDown);
 });
